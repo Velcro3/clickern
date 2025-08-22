46b3bc5f873278920a57d22c8d8ac56f25ffb8cb
Viewer <leoncatpl27272@proton.me>
2025-08-22 22:41:43 +0100

    Add ignition.c

---
+++ ignition.c
@@ -0,0 +1,7 @@
+// Ignition.c
+// Boot payload for Clickern.
+// Copyright (c) 2025 Viewer. All Rights Reserved.
+// This software is licensed under MIT. You must include this header IN FULL if distributing/modifying code.
+// If distributing the binary, you MUST specify that a form of the code was used.
+// If you don't do this, YOU ARE *COMMITING COPYRIGHT INFRINGMENT*.
+
