57225453d4e98043026520e48f1c974f7852235f
Viewer <leoncatpl27272@proton.me>
2025-08-22 23:10:05 +0100

    Add starter.c

---
+++ starter.c
@@ -0,0 +1,13 @@
+// starter.c
+// EFI payload to load ignition.c.
+// Licensed under MIT.
+// Copyright (c) 2025 Viewer. All Rights Reserved.
+#include <efi.h>
+#include <efilib.h>
+EFI_STATUS
+  efi_main(EFI_HANDLE ImageHandle,
+           EFI_SYSTEM_TABLE *SystemTable) {
+  InitializeLib(ImageHandle, SystemTable);
+  Print(L"Starter.efi payload loaded.\n");
+  return EFI_SUCCESS
+}
